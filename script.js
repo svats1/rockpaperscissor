ef4e51ee48bb51ece7899d784f684716c1178431
stallionblade <119752878+stallionblade@users.noreply.github.com>
2022-12-26 11:11:02 -0500

    Create reset logic, game functional

--- script.js
+++ script.js
@@ -1,101 +1,92 @@
+let playerScore = 0
+let computerScore = 0
+
 function getComputerChoice(){
     const choices = ["rock", "paper", "scissor"]
     const index = Math.floor((Math.random()*3))
     return (choices[index])
 }
 
-let playerScore = 0
-let computerScore = 0
-
+function logScore() {
+    console.log("Player Score is "  + playerScore)
+    console.log("Computer Score is " + computerScore)
+}
 function playRound(playerSelection){
     const computerChoice = getComputerChoice()
     if (playerSelection == "rock"){
         if (computerChoice == playerSelection){
             console.log(`You: ${playerSelection} Computer ${computerChoice} - It's a Tie!`)
-            console.log("Player Score is "  + playerScore)
-            console.log("Computer Score is " + computerScore)
+            logScore()
         }
         else if (computerChoice == "scissor") {
             console.log(`You: ${playerSelection} Computer ${computerChoice} - You win!`)
             playerScore += 1
-            console.log("Player Score is "  + playerScore)
-            console.log("Computer Score is " + computerScore)
+            logScore()
         }
         else {
             console.log(`You: ${playerSelection} Computer ${computerChoice} - You lose.`)
             computerScore += 1
-            console.log("Player Score is "  + playerScore)
-            console.log("Computer Score is " + computerScore)
+            logScore()
         }
     }
     else if (playerSelection == "paper"){
         if (computerChoice == playerSelection){
             console.log(`You: ${playerSelection} Computer ${computerChoice} - It's a Tie!`)
-            console.log("Player Score is "  + playerScore)
-            console.log("Computer Score is " + computerScore)
+            logScore()
         }
         else if (computerChoice == "rock") {
             console.log(`You: ${playerSelection} Computer ${computerChoice} - You win!`)
             playerScore += 1
-            console.log("Player Score is "  + playerScore)
-            console.log("Computer Score is " + computerScore)
+            logScore()
         }
         else {
             console.log(`You: ${playerSelection} Computer ${computerChoice} - You lose.`)
             computerScore += 1
-            console.log("Player Score is "  + playerScore)
-            console.log("Computer Score is " + computerScore)
+            logScore()
         }
     }
     else if (playerSelection == "scissor"){
         if (computerChoice == playerSelection){
             console.log(`You: ${playerSelection} Computer ${computerChoice} - It's a Tie!`)
-            console.log("Player Score is "  + playerScore)
-            console.log("Computer Score is " + computerScore)
+            logScore()
         }
         else if (computerChoice == "paper") {
             console.log(`You: ${playerSelection} Computer ${computerChoice} - You win!`)
             playerScore += 1
-            console.log("Player Score is "  + playerScore)
-            console.log("Computer Score is " + computerScore)
+            logScore()
         }
         else {
             console.log(`You: ${playerSelection} Computer ${computerChoice} - You lose.`)
             computerScore += 1
-            console.log("Player Score is "  + playerScore)
-            console.log("Computer Score is " + computerScore)
+            logScore()
         }
     }
-    else {
-        alert("Improper value")
+    if (!(playerScore < 5 && computerScore < 5)) {
+        if (alert('GAME OVER')){}
+        else window.location.reload()
     }
 }
 
-// if (playerScore || computerScore >5 ) {
-//     console.log('GAME OVER')
-//     console.log("Player Score is "  + playerScore)
-//     console.log("Computer Score is " + computerScore)
-// }
-
+// Grab div to hold all buttons
 const choices = document.querySelector('.choices')
 
+// Create Rock element
 const rock = document.createElement('button')
 rock.className = "rockBtn"
 rock.innerText = "ROCK"
 rock.addEventListener('click', () => playRound("rock"))
 choices.appendChild(rock)
 
+// Create Paper element
 const paper = document.createElement('button')
 paper.className = "paperBtn"
 paper.innerText = "PAPER"
 paper.addEventListener('click', () => playRound("paper"))
 choices.appendChild(paper)
 
+// Create Scissor element
 const scissor = document.createElement('button')
 scissor.className = "scissorBtn"
 scissor.innerText = "SCISSOR"
 scissor.addEventListener('click', () => playRound("scissor"))
 choices.appendChild(scissor)
-
-console.log("Player Score is "  + playerScore)
-console.log("Computer Score is " + computerScore)
